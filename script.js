6c66b6fb11886020e9faabf4cb18c7f5197f2a73
KevinKhuu27 <khuu.kevin27@hotmail.com>
2024-05-30 15:23:01 -0400

    added password validation

---
+++ script.js
@@ -0,0 +1,28 @@
+const password = document.getElementById('password');
+const confirm = document.getElementById('confirm');
+const message = document.getElementById('message');
+
+function validatePassword() {
+    if (password.value !== confirm.value) 
+    {
+        message.textContent = "Password does not match";
+        return false;
+    }
+    message.textContent = "";
+    return true;
+}
+
+password.addEventListener('input', validatePassword);
+confirm.addEventListener('input', validatePassword);
+
+document.getElementById('signupForm').addEventListener('submit', function(event) 
+{
+    if (!validatePassword()) 
+    {
+        event.preventDefault();
+    }
+    else 
+    {
+        alert("Registration successful!")
+    }
+});
